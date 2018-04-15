769e805e838644b6bdd0f2761b44c3341813b675
鄭小玟 <shihwen225@ymail.com>
2018-04-15 13:18:13 +0800

    4/15 修改完畢

---
+++ homework3-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  homework3-1
+//
+//  Created by shih wen Cheng on 2018/3/21.
+//  Copyright © 2018年 shih wen Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
